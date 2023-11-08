01a89eda8a26e69320e9540ae5e997cb6721365b
Xiao Zhang <58770171+xiao11lam@users.noreply.github.com>
2023-11-08 08:25:35 -0800

    add the more specific stuff

--- app.py
+++ app.py
@@ -12,19 +12,26 @@ app = Flask(__name__)
 def index_get():
     return render_template("base.html")
 
-@app.post("/predict")
+
+def get_rule_based_response(msg):
+    default_answer = '<div>Leave your email, we will help you:</div>' + \
+      '<input type="email" id="email-input" placeholder="Your email" style="border: 1px solid #f9f9f9; border-radius: 5px; padding: 8px; margin-top: 10px; font-size: 14px; width: 100%; margin-right: 5px;"> ' + \
+      '<button class="submit-email-button" style="background-color: #d8acfc; color: #fff; padding: 8px 16px; border: none; border-radius: 5px; cursor: pointer; margin-top: 10px;" onclick="submitEmail()">Submit</button>'
+
+    return default_answer
+
+@app.route("/predict", methods=["POST"])
 def predict():
     text = request.get_json().get("message")
-    response = get_response(text)
+    response = get_rule_based_response(text)
+    data = request.get_json()
+    email = data.get("email")
+    print(email)
 
-#     if response == "I do not understand...":
-#         response = "Leave your email, we will help you?"
-
-
-    if "@" in text:
-        response = "Thank you for your email. We'll be in touch as soon as possible!"
-
-
+    # Check if email information exists (not an empty string)
+    if email:
+        # If the email information exists, change the response
+        response = "great day"
 
     message = {"answer": response}
     return jsonify(message)
--- static/app.js
+++ static/app.js
@@ -1,5 +1,9 @@
 
 // Define the submitEmail() function
+
+let isEmail = false;
+
+
 function submitEmail() {
     // Capture the email entered by the user
     const emailInput = document.getElementById('email-input');
@@ -12,11 +16,32 @@ function submitEmail() {
     // Disable the button so the user cannot click it again
     button.disabled = true;
 
-    // Hide the email input field
-    emailInput.style.display = 'none'; // or you can use emailInput.style.display = 'hidden'; to hide it
-}
 
-// Your existing code...
+    // Send the email information to the backend
+    fetch('/predict', {
+        method: 'POST',
+        body: JSON.stringify({ email }),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+        .then(response => {
+            if (response.ok) {
+
+                console.log('Email information sent to the backend');
+                // Add a message to the chatbox
+                const emailSentMessage = { name: "Marita", message: "Email information sent to the backend" };
+                chatbox.messages.push(emailSentMessage);
+                chatbox.updateChatText();
+
+            } else {
+                // Handle any errors here
+                console.error('Error sending email information to the backend');
+            }
+        });
+
+
+}
 
 
 
@@ -30,6 +55,8 @@ class Chatbox {
 
         this.state = false;
         this.messages = [];
+        this.promptDisplayed = false; // Flag to track if the initial prompt has been shown
+
     }
 
     display() {
@@ -51,81 +78,105 @@ class Chatbox {
         this.state = !this.state;
 
         // show or hides the box
-        if(this.state) {
-            chatbox.classList.add('chatbox--active')
+        if (this.state) {
+            chatbox.classList.add('chatbox--active');
+
+            if (!this.promptDisplayed) {
+                // Display the initial prompt
+                this.messages.push({ name: "Marita", message: "Hi, I am your free virtual health assistant provided by Dr. O’Dwyer from the Beacon Hospital. Please take a moment to read our consent and privacy form at [Consent URL].\n" +
+                        "If you agree to the consent and privacy policy and to receive health updates via SMS, please reply 'Y'" });
+                this.promptDisplayed = true;
+                this.updateChatText(chatbox);
+            }
         } else {
-            chatbox.classList.remove('chatbox--active')
+            chatbox.classList.remove('chatbox--active');
         }
     }
 
-    onSendButton(chatbox) {
-        var textField = chatbox.querySelector('input');
-        let text1 = textField.value
+
+    onSendButton
+    (chatbox) {
+        var textField = document.getElementById('resultPanel');
+        let text1 = textField.value;
         if (text1 === "") {
             return;
         }
 
+        console.log(text1)
         let msg1 = { name: "User", message: text1 }
         this.messages.push(msg1);
-        console.log(msg1)
+        // console.log(msg1)
 
-        fetch('http://127.0.0.1:5000/predict', {
+
+        fetch('/predict', {
             method: 'POST',
             body: JSON.stringify({ message: text1 }),
             mode: 'cors',
             headers: {
-              'Content-Type': 'application/json'
+                'Content-Type': 'application/json'
             },
-          })
-          .then(r => r.json())
+        })
+            .then(r => r.json())
 
 
+            .then(r => {
+                // console.log(r.answer)
+                // if (r.answer === "I do not understand...") {
+                //     // If the response is "I do not understand...", create an email input form
+                //     r.answer = '<div>Leave your email, we will help you:</div>' +
+                //         '<input type="email" id="email-input" placeholder="Your email" style="border: 1px solid #f9f9f9; border-radius: 5px; padding: 8px; margin-top: 10px; font-size: 14px; width: 100%; margin-right: 5px;"> ' +
+                //         '<button class="submit-email-button" style="background-color: #d8acfc; color: #fff; padding: 8px 16px; border: none; border-radius: 5px; cursor: pointer; margin-top: 10px;" onclick="submitEmail()">Submit</button>'
+                //
+                //
+                //
+                // }
+                // if (isEmail == true) {
+                //     // If the response is "I do not understand...", create an email input form
+                //     r.answer = '<div>Leave computer, we will help you:</div>' +
+                //         '<input type="email" id="email-input" placeholder="Your email" style="border: 1px solid #f9f9f9; border-radius: 5px; padding: 8px; margin-top: 10px; font-size: 14px; width: 100%; margin-right: 5px;"> ' +
+                //         '<button class="submit-email-button" style="background-color: #d8acfc; color: #fff; padding: 8px 16px; border: none; border-radius: 5px; cursor: pointer; margin-top: 10px;" onclick="submitEmail()">Submit</button>'
+                //
+                // }
 
-          .then(r => {
+                let msg2 = { name: "Marita", message: r.answer };
+                this.messages.push(msg2);
+                this.updateChatText(chatbox)
+                textField.value = ''
 
-          if (r.answer === "I do not understand...") {
-              // If the response is "I do not understand...", create an email input form
-              r.answer = '<div>Leave your email, we will help you:</div>' +
-                  '<input type="email" id="email-input" placeholder="Your email" style="border: 1px solid #f9f9f9; border-radius: 5px; padding: 8px; margin-top: 10px; font-size: 14px; width: 100%; margin-right: 5px;"> ' +
-                  '<button class="submit-email-button" style="background-color: #d8acfc; color: #fff; padding: 8px 16px; border: none; border-radius: 5px; cursor: pointer; margin-top: 10px;" onclick="submitEmail()">Submit</button>'
+                // console.log(msg2)
 
-
-
-          }
-
-
-
-            let msg2 = { name: "Marita", message: r.answer };
-            this.messages.push(msg2);
-            this.updateChatText(chatbox)
-            textField.value = ''
-
-            console.log(msg2)
-
-          }).catch((error) => {
+            }).catch((error) => {
             console.error('Error:', error);
             this.updateChatText(chatbox)
             textField.value = ''
-          });
+        });
+
+
     }
 
-    updateChatText(chatbox) {
+    // Make sure that this method is working correctly
+    updateChatText() {
+        if (!this.args.chatBox) {
+            console.error('chatBox element is not correctly selected.');
+            return;
+        }
+
         var html = '';
         this.messages.slice().reverse().forEach(function(item, index) {
-            if (item.name === "Marita")
-            {
-                html += '<div class="messages__item messages__item--visitor">' + item.message + '</div>'
+            if (item.name === "Marita") {
+                html += '<div class="messages__item messages__item--visitor">' + item.message + '</div>';
+            } else {
+                html += '<div class="messages__item messages__item--operator">' + item.message + '</div>';
             }
-            else
-            {
-                html += '<div class="messages__item messages__item--operator">' + item.message + '</div>'
-            }
-          });
+        });
 
-        const chatmessage = chatbox.querySelector('.chatbox__messages');
+        const chatmessage = this.args.chatBox.querySelector('.chatbox__messages');
         chatmessage.innerHTML = html;
     }
 }
 
 const chatbox = new Chatbox();
+
+// Just show the prompt directly without clicking
+chatbox.toggleState(chatbox.args.chatBox);
 chatbox.display();
